4ff29dec556a250338aa027fe6c966e481a7698a
dshikuta <dshikuta@gmail.com>
2023-07-13 22:27:04 +0000

    MySQL advanced

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,10 @@
---a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, SUM(fans) AS nb_fans
-    FROM metal_bands
-    GROUP BY origin
-    ORDER BY nb_fans DESC;
+-- SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+SELECT
+  origin,
+  SUM(fans) As nb_fans
+FROM
+  metal_bands
+GROUP BY
+  origin
+ORDER BY
+  nb_fans DESC;
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,10 @@
-SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT
+  band_name,
+  IFNULL (split, 2022) - formed As lifespan
+FROM
+  metal_bands
+WHERE
+  style LIKE '%Glam rock%'
+ORDER BY
+  lifespan DESC;
